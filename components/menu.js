cb8f6db1e3f9b168b9b335af171ffc0f866b803c
Gary Buckle <buckle.g@mac.com>
2021-11-04 23:05:12 +0000

    Enabled Link

--- components/menu.js
+++ components/menu.js
@@ -4,22 +4,25 @@ import Link from "next/link";
 export default function Menu() {
   return (
     <>
-      <ul class="flex mt-6 justify-center">
-        <li class="mr-6">
-          <a class="text-gray-500 hover:text-blue-800" href="#">
-            Blog
-          </a>
-        </li>
-        <li class="mr-6">
-          <a class="text-gray-500 hover:text-blue-800" href="#">
-            Photos
-          </a>
-        </li>
-        <li class="mr-6">
+      <ul class="flex mt-6 justify-center text-2xl">
+        <li class="mr-8">
           <a class="text-gray-500 hover:text-blue-800" href="#">
             Work
           </a>
         </li>
+        <li class="mr-8">
+          <a class="text-gray-500 hover:text-blue-800" href="#">
+            Rest
+          </a>
+        </li>
+        <li class="mr-8">
+          <a class="text-gray-500 hover:text-blue-800" href="#">
+            Play
+          </a>
+        </li>
+        <Link href="/blog">
+          <a>Articles</a>
+        </Link>
       </ul>
     </>
   );
